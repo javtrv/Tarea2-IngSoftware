0322fac6f41709933c80e0b3625d83afcb7a8267
javtrv <jav393@gmail.com>
2018-10-09 15:23:44 -0400

    Se creo el archivo de prueba Pension.py con el metodo RecibePension

---
+++ Pension.py
@@ -0,0 +1,6 @@
+import time
+
+class Pension:
+
+    def RecibePension(self):
+        return 0
--- testPension.py
+++ testPension.py
@@ -1,10 +1,10 @@
 import unittest
-
+from Pension import * 
 class testPension(unittest.TestCase):
     
     def test_RecibePension(self):
         p = Pension()
-        self.assertEqual(self,0, p.RecibePension)
+        self.assertEqual(0, p.RecibePension())
 
 
 if __name__=="__main__":
