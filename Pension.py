f5eac7ec774e5c396f6198f886b64351b4c1c73c
javtrv <jav393@gmail.com>
2018-10-09 17:45:16 -0400

    Se agrego a Pension.py el metodo 'compararFecha' el cual prueba que la fecha ingresada por el usuario no sea mayor que la fecha actual del sistema

--- Pension.py
+++ Pension.py
@@ -2,5 +2,18 @@ import time
 
 class Pension:
 
-    def RecibePension(self):
+    def RecibePension(self, anhoUsuario):
         return 0
+    
+    def CompararFecha(self, anhoUsuario):
+        anhoActual = time.strftime("%Y")
+        if anhoActual < anhoUsuario:
+            return 1
+        else:
+            return 0
+
+#p = Pension()
+#fechaUsuario = input("Ingrese la fecha de nacimiento (dd/mm/aaaa): ")
+#anhoUsuario = fechaUsuario.split("/") #Se crea un vector de la forma [dd,mm,aaaa]
+#anhoUsuario = anhoUsuario[2]
+#p.ObtenerFecha()
--- testPension.py
+++ testPension.py
@@ -6,9 +6,17 @@ class testPension(unittest.TestCase):
         p = Pension()
         self.assertEqual(0, p.RecibePension())
 
-    def test_Fecha_Ingresada_Mayor_Fecha_Actual(self):
+    def test_Fecha_Ingresada_Mayor_Fecha_Actual1(self):
         p = Pension()
-        self.assertEqual(1,ObtenerFecha())
+        self.assertEqual(0,p.CompararFecha(1995))
+
+    def test_Fecha_Ingresada_Mayor_Fecha_Actual2(self):
+        p = Pension()
+        self.assertEqual(0,p.CompararFecha(2007))
+
+    def test_Fecha_Ingresada_Mayor_Fecha_Actual3(self):
+        p = Pension()
+        self.assertEqual(0,p.CompararFecha(2020))
 
 
 if __name__=="__main__":
