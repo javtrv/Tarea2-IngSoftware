734cf0ef37702a1ffcd7d815a187fceb18a17c09
javtrv <jav393@gmail.com>
2018-10-09 19:18:01 -0400

    Se modifico en  Pension.py el metodo RecibePension el cual determina si un empleado recibe pension segun sus caractericas, se agrego el parametro anhosInsalubres

--- Pension.py
+++ Pension.py
@@ -2,26 +2,48 @@ import time
 
 class Pension:
 
-    def RecibePension(self, anhoUsuario,sexo,horasCotizadas):
+    def RecibePension(self, anhoUsuario,sexo,horasCotizadas, anhosInsalubres):
         anhoActual = time.strftime("%Y")
-        if self.CompararFecha(anhoUsuario) == 0:
-            if sexo == 'm':
-                if (int(anhoActual) - anhoUsuario) >= 60:
-                    if horasCotizadas >= 750:
-                        return 0
+        if anhosInsalubres < 4:
+            if self.CompararFecha(anhoUsuario) == 0:
+                if sexo == 'm':
+                    if (int(anhoActual) - anhoUsuario) >= 60:
+                        if horasCotizadas >= 750:
+                            return 0
+                        else:
+                            return 1
                     else:
                         return 1
-                else:
-                    return 1
-            elif sexo == 'f':
-                if (int(anhoActual) - anhoUsuario) >= 55:
-                    if horasCotizadas >= 750:
-                        return 0
+                elif sexo == 'f':
+                    if (int(anhoActual) - anhoUsuario) >= 55:
+                        if horasCotizadas >= 750:
+                            return 0
+                        else:
+                            return 1
                     else:
                         return 1
-                else:
-                    return 1
-    
+        else:
+            anhosRebajados = anhosInsalubres / 4
+            if anhosRebajados > 5:
+                anhosRebajados = 5
+            if self.CompararFecha(anhoUsuario) == 0:
+                if sexo == 'm':
+                    if (int(anhoActual) - anhoUsuario) >= 60 - anhosRebajados:
+                        if horasCotizadas >= 750:
+                            return 0
+                        else:
+                            return 1
+                    else:
+                        return 1
+                elif sexo == 'f':
+                    if (int(anhoActual) - anhoUsuario) >= 55 - anhosRebajados:
+                        if horasCotizadas >= 750:
+                            return 0
+                        else:
+                            return 1
+                    else:
+                        return 1
+
     def CompararFecha(self, anhoUsuario):
         anhoActual = time.strftime("%Y")
         if int(anhoActual) < anhoUsuario:
