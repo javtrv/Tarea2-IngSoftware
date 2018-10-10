754c12ffa457a54c3813b52e7b96fe26a6288d99
Andrés Ignacio Torres <andresitorresm@gmail.com>
2018-10-10 12:01:26 -0400

    Agrega pruebas de malicia para AnhoMayorQueActual y comentarios

--- testPension.py
+++ testPension.py
@@ -1,30 +1,123 @@
-import unittest
-from Pension import Pension
+import unittest # Modulo para realizar pruebas unitarias
+from Pension import Pension # Modulo al cual se le haran las pruebas
 
-class PruebasEdadMayor(unittest.TestCase):
+class PruebasAnhoMayor(unittest.TestCase):
+    '''
+    Conjunto de pruebas de la funcion anho mayor,
+    de frontera y de esquina
+    '''
+
+    # Configuracion
+
     def setUp(self):
-        self.p = Pension()
-
-    def tearDown(self):
-        self.p = None
-
-##### PRUEBAS DE EDAD MAYOR QUE ANHO ACTUAL
-
-    def test_Fecha_Ingresada_Mayor_Fecha_Actual1(self):
-        self.assertFalse(self.p.anho_mayor_que_actual(0))
-
-    def test_Fecha_Ingresada_Mayor_Fecha_Actual2(self):
-        self.assertFalse(self.p.anho_mayor_que_actual(2018))
-
-    def test_Fecha_Ingresada_Mayor_Fecha_Actual3(self):
-        self.assertTrue(self.p.anho_mayor_que_actual(2020))
-
-class PruebasCalcularEdad(unittest.TestCase):
-    def setUp(self):
+        '''
+        Instancia el modulo Pension y configura el anho actual
+        de manera directa para poder controlar las pruebas
+        '''
         self.p = Pension()
         self.p.anho_actual = 2018
 
     def tearDown(self):
+        '''
+        Elimina la instancia del modulo Pension
+        asociada a la prueba
+        '''
+        self.p = None
+
+    # Pruebas
+
+    def test_AnhoMayorQueAnho0(self):
+        '''
+        Prueba: Ejecutar la funcion con 0 como argumento
+        Resultado esperado: False
+        '''
+        self.assertFalse(self.p.anho_mayor_que_actual(0))
+
+    def test_AnhoMayorQueAnho2017(self):
+        '''
+        Prueba: Ejecutar la funcion con 2017 como argumento
+        Resultado esperado: False
+        '''
+        self.assertFalse(self.p.anho_mayor_que_actual(2017))
+
+    def test_AnhoMayorQueAnho2018(self):
+        '''
+        Prueba: Ejecutar la funcion con 2018 como argumento
+        Resultado esperado: False
+        '''
+        self.assertFalse(self.p.anho_mayor_que_actual(2018))
+
+    def test_AnhoMayorQueAnho2019(self):
+        '''
+        Prueba: Ejecutar la funcion con 2019 como argumento
+        Resultado esperado: True
+        '''
+        self.assertTrue(self.p.anho_mayor_que_actual(2019))
+
+    def test_AnhoMayorQueAnho2020(self):
+        '''
+        Prueba: Ejecutar la funcion con 2020 como argumento
+        Resultado esperado: True
+        '''
+        self.assertTrue(self.p.anho_mayor_que_actual(2020))
+
+    def test_AnhoMayorQueAnho100000(self):
+        '''
+        Prueba: Ejecutar la funcion con 100000 como argumento
+        Resultado esperado: True
+        '''
+        self.assertTrue(self.p.anho_mayor_que_actual(100000))
+
+class PruebasAnhoMayorMalicia(unittest.TestCase):
+    '''
+    Conjunto de pruebas de malicia de la funcion anho mayor
+    '''
+
+    # Configuracion
+
+    def setUp(self):
+        '''
+        Instancia el modulo Pension
+        '''
+        self.p = Pension()
+
+    def tearDown(self):
+        '''
+        Elimina la instancia del modulo Pension
+        asociada a la prueba
+        '''
+        self.p = None
+
+    # Pruebas
+
+    def test_AnhoSinParametros(self):
+        '''
+        Prueba: Ejecutar la funcion sin argumentos
+        Resultado esperado: Excepcion TypeError
+        '''
+        self.assertRaises(TypeError, self.p.anho_mayor_que_actual)
+
+    def test_AnhoConParametroString(self):
+        '''
+        Prueba: Ejecutar la funcion con un argumento string
+        Resultado esperado: Excepcion TypeError
+        '''
+        self.assertRaises(TypeError, self.p.anho_mayor_que_actual, '2010')
+
+class PruebasCalcularEdad(unittest.TestCase):
+    def setUp(self):
+        '''
+        Instancia el modulo Pension y configura el anho actual
+        de manera directa para poder controlar las pruebas
+        '''
+        self.p = Pension()
+        self.p.anho_actual = 2018
+
+    def tearDown(self):
+        '''
+        Elimina la instancia del modulo Pension
+        asociada a la prueba
+        '''
         self.p = None
 
     def test_Calcula_Edad_Cero(self):
@@ -44,12 +137,17 @@ class PruebasCalcularEdad(unittest.TestCase):
 
 class PruebasDeRequisitos(unittest.TestCase):
     def setUp(self):
+        '''
+        Instancia el modulo Pension
+        '''
         self.p = Pension()
 
     def tearDown(self):
+        '''
+        Elimina la instancia del modulo Pension
+        asociada a la prueba
+        '''
         self.p = None
-
-##### PRUEBAS DE REQUISITOS EN recibe_pension
 
     def test_RecibePension1(self):
         self.assertFalse(self.p.recibe_pension(2012, 'f', 750, 0))
@@ -64,23 +162,30 @@ class PruebasDeRequisitos(unittest.TestCase):
         self.assertTrue(self.p.recibe_pension(1950, 'm', 1000, 0))
 
     def test_RecibePension6(self):
-    	self.assertTrue(self.p.recibe_pension(1958, 'm', 750, 0))
+        self.assertTrue(self.p.recibe_pension(1958, 'm', 750, 0))
     def test_RecibePension7(self):
-    	self.assertTrue(self.p.recibe_pension(1963, 'f', 750, 0))
+        self.assertTrue(self.p.recibe_pension(1963, 'f', 750, 0))
 
     def test_RecibePension8(self):
-    	self.assertFalse(self.p.recibe_pension(1959, 'm', 750, 0))
+        self.assertFalse(self.p.recibe_pension(1959, 'm', 750, 0))
     def test_RecibePension9(self):
-    	self.assertFalse(self.p.recibe_pension(1964, 'f', 750, 0))
+        self.assertFalse(self.p.recibe_pension(1964, 'f', 750, 0))
 
     def test_RecibePension10(self):
-    	self.assertFalse(self.p.recibe_pension(0, '', 0, 0))
+        self.assertFalse(self.p.recibe_pension(0, '', 0, 0))
 
 class PruebasInsalubridad(unittest.TestCase):
     def setUp(self):
+        '''
+        Instancia el modulo Pension
+        '''
         self.p = Pension()
 
     def tearDown(self):
+        '''
+        Elimina la instancia del modulo Pension
+        asociada a la prueba
+        '''
         self.p = None
 
     def test_Recibe_Pension_Sin_Insalubridad(self):
@@ -94,9 +199,16 @@ class PruebasInsalubridad(unittest.TestCase):
 
 class PruebasMaliciaSexo(unittest.TestCase):
     def setUp(self):
+        '''
+        Instancia el modulo Pension
+        '''
         self.p = Pension()
 
     def tearDown(self):
+        '''
+        Elimina la instancia del modulo Pension
+        asociada a la prueba
+        '''
         self.p = None
 
     def test_Introduce_Letra_Incorrecta1(self):
