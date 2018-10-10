f8f9c5b59be3455091a010c8150c6afc50a2099c
Andrés Ignacio Torres <andresitorresm@gmail.com>
2018-10-10 10:12:55 -0400

    Refactoriza codigo para incluir funcion de edad

--- Pension.py
+++ Pension.py
@@ -3,12 +3,11 @@ import time
 class Pension:
     anhoActual = int(time.strftime("%Y"))
 
-    def RecibePension(self, anhoUsuario,sexo,horasCotizadas, anhosInsalubres):
-        anhoActual = time.strftime("%Y")
+    def RecibePension(self, anhoUsuario, sexo, horasCotizadas, anhosInsalubres):
         if anhosInsalubres < 4:
             if self.anho_mayor_que_actual(anhoUsuario) == 0:
                 if sexo == 'm':
-                    if (int(anhoActual) - anhoUsuario) >= 60:
+                    if self.calcular_edad(anhoUsuario) >= 60:
                         if horasCotizadas >= 750:
                             return False
                         else:
@@ -16,7 +15,7 @@ class Pension:
                     else:
                         return True
                 elif sexo == 'f':
-                    if (int(anhoActual) - anhoUsuario) >= 55:
+                    if self.calcular_edad(anhoUsuario) >= 55:
                         if horasCotizadas >= 750:
                             return False
                         else:
@@ -29,7 +28,7 @@ class Pension:
                 anhosRebajados = 5
             if self.anho_mayor_que_actual(anhoUsuario) == 0:
                 if sexo == 'm':
-                    if (int(anhoActual) - anhoUsuario) >= 60 - anhosRebajados:
+                    if self.calcular_edad(anhoUsuario) >= 60 - anhosRebajados:
                         if horasCotizadas >= 750:
                             return False
                         else:
@@ -37,7 +36,7 @@ class Pension:
                     else:
                         return True
                 elif sexo == 'f':
-                    if (int(anhoActual) - anhoUsuario) >= 55 - anhosRebajados:
+                    if self.calcular_edad(anhoUsuario) >= 55 - anhosRebajados:
                         if horasCotizadas >= 750:
                             return False
                         else:
@@ -51,7 +50,7 @@ class Pension:
     def calcular_edad(self, anho_nacimiento):
         if anho_nacimiento > self.anhoActual:
             return 0
-            
+
         return self.anhoActual - anho_nacimiento
 
 
