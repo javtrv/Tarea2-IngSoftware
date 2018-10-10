4dfbf55b4a5efa2c87c02dc488806bb75826f208
Andrés Ignacio Torres <andresitorresm@gmail.com>
2018-10-10 09:55:36 -0400

    Separa los casos de prueba en clases distintas

--- testPension.py
+++ testPension.py
@@ -1,20 +1,31 @@
 import unittest
-from Pension import * 
-class testPension(unittest.TestCase):
+from Pension import Pension
+
+class PruebasEdadMayor(unittest.TestCase):
     def setUp(self):
         self.p = Pension()
+
+    def tearDown(self):
+    	self.p = None
 
 ##### PRUEBAS DE EDAD MAYOR QUE ANHO ACTUAL
 
     def test_Fecha_Ingresada_Mayor_Fecha_Actual1(self):
-        self.assertEqual(0,self.p.CompararFecha(0))
+        self.assertEqual(0, self.p.CompararFecha(0))
 
     def test_Fecha_Ingresada_Mayor_Fecha_Actual2(self):
-        self.assertEqual(0,self.p.CompararFecha(2018))
+        self.assertEqual(0, self.p.CompararFecha(2018))
 
     def test_Fecha_Ingresada_Mayor_Fecha_Actual3(self):
-        self.assertEqual(1,self.p.CompararFecha(2020))
+        self.assertEqual(1, self.p.CompararFecha(2020))
 
+
+class PruebasDeRequisitos(unittest.TestCase):
+    def setUp(self):
+        self.p = Pension()
+
+    def tearDown(self):
+    	self.p = None
 
 ##### PRUEBAS DE REQUISITOS EN RecibePension
 
@@ -28,7 +39,6 @@ class testPension(unittest.TestCase):
         self.assertEqual(1, self.p.RecibePension(1950, 'm', 200))
     def test_RecibePension5(self):
         self.assertEqual(0, self.p.RecibePension(1950, 'm', 1000))
-
 
 
 if __name__=="__main__":
