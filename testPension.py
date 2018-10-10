af1d21554c0dbd5b8ac68105044aa34e8d3be071
Andrés Ignacio Torres <andresitorresm@gmail.com>
2018-10-10 10:08:21 -0400

    Agrega pruebas para calcular edad en funcion

--- testPension.py
+++ testPension.py
@@ -19,6 +19,28 @@ class PruebasEdadMayor(unittest.TestCase):
     def test_Fecha_Ingresada_Mayor_Fecha_Actual3(self):
         self.assertTrue(self.p.anho_mayor_que_actual(2020))
 
+class PruebasCalcularEdad(unittest.TestCase):
+    def setUp(self):
+        self.p = Pension()
+        self.p.anho_actual = 2018
+
+    def tearDown(self):
+    	self.p = None
+
+    def test_Calcula_Edad_Cero(self):
+    	self.assertEqual(0, self.p.calcular_edad(2018))
+
+    def test_Calcula_Edad_10(self):
+    	self.assertEqual(10, self.p.calcular_edad(2008))
+
+    def test_Calcula_Edad_21(self):
+    	self.assertEqual(21, self.p.calcular_edad(1997))
+
+    def test_Calcula_Edad_No_Nacido(self):
+    	self.assertEqual(0, self.p.calcular_edad(2027))
+
+    def test_Calcula_Edad_Anho_0(self):
+    	self.assertEqual(2018, self.p.calcular_edad(0))
 
 class PruebasDeRequisitos(unittest.TestCase):
     def setUp(self):
@@ -35,7 +57,7 @@ class PruebasDeRequisitos(unittest.TestCase):
         self.assertTrue(self.p.RecibePension(2000, 'm', 2131))
     def test_RecibePension3(self):
         self.assertTrue(self.p.RecibePension(1950, 'm', 200))
-        
+
     def test_RecibePension4(self):
         self.assertFalse(self.p.RecibePension(1945, 'f', 750))
     def test_RecibePension5(self):
